b460337bade2e7058a12375e0cdfaa7b44140541
wiknwo <spinningthewww@outlook.com>
2022-03-15 00:01:21 +0000

    Correct

---
+++ The Core/willyou.py
@@ -0,0 +1,14 @@
+def willYou(young, beautiful, loved):
+    """
+    Once Mary heard a famous song, and a line from 
+    it stuck in her head. That line was "Will you still 
+    love me when I'm no longer young and beautiful?". 
+    Mary believes that a person is loved if and only if 
+    he/she is both young and beautiful, but this is quite 
+    a depressing thought, so she wants to put her belief 
+    to the test.
+
+    Knowing whether a person is young, beautiful and loved, 
+    find out if they contradict Mary's belief.
+    """
+    return (young and beautiful) != loved
